3bf2741936fb22f0b40b7ad9f1980614a532421b
Daranix <Daranix@users.noreply.github.com>
2024-01-07 02:28:37 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,6 @@ echo "Velocity build: $VELOCITY_BUILD"
 mkdir -p data
 curl https://api.papermc.io/v2/projects/velocity/versions/$VELOCITY_VERSION/builds/$VELOCITY_BUILD/downloads/velocity-$VELOCITY_VERSION-$VELOCITY_BUILD.jar -o ./data/velocity.jar
 cd data
-java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
+java -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
 
 
